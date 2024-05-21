45923203100ff6afd9cc84a57ca57985a04a2629
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2024-05-21 14:41:44 +0545

    checking deploy site contact

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -3,10 +3,10 @@ import nodemailer from "nodemailer";
 
 export async function POST(req) {
   try {
-    const body = await req.json(); // Parse the request body
+    console.log("Incoming request:", req);
+    const body = await req.json();
+    console.log("Parsed body:", body);
     const { name, email, phone, subject, message } = body;
-
-    // console.log(name, email, phone, subject, message); // Check if data is received correctly
 
     const transporter = nodemailer.createTransport({
       host: "smtp.mailtrap.io",
@@ -24,7 +24,8 @@ export async function POST(req) {
       text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`,
     };
 
-    await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info);
     return NextResponse.json(
       {
         message: "Email sent successfully",
@@ -32,7 +33,7 @@ export async function POST(req) {
       { status: 200 }
     );
   } catch (error) {
-    console.error(error);
+    console.error("Error:", error);
     return NextResponse.json(
       {
         error: "Error sending email",
